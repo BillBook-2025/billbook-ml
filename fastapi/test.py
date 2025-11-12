0f106f7a142d97926bd67abc2d9b9f172db4673c
DESKTOP-DQN8BQ4\golje <https://github.com/000uk>
2025-11-13 03:16:55 +0900

    feat: 배치단위로 임베딩할 수 있도록 변경

--- fastapi/test.py
+++ fastapi/test.py
@@ -1,12 +1,22 @@
 from services import AladinBookFetchService, VectorDBService
-from ml_models import e5_embedding
+from ml_models import E5Embedding
 
 if __name__ == "__main__":
     TTB_KEY = "ttbseoll770145001"
-    service = AladinBookFetchService(ttb_key=TTB_KEY)
+    aladin_service = AladinBookFetchService(ttb_key=TTB_KEY)
+    e5_service = E5Embedding()
+    vertor_db_service = VectorDBService()
 
-    bestsellers = service.fetch_books(query_type="Bestseller", max_results=5)
+    # 책 불러오기
+    bestsellers = aladin_service.fetch_books(query_type="Bestseller", max_results=50)
 
-    for idx, book in enumerate(bestsellers, start=1):    
-        service = e5_embedding()
-        result = service.embed_batch(book)
+    # 임베딩 하기
+    # 배치 텍스트와 book_id를 같은 순서로 저장하니 배치단위로 처리해도 매칭 안전함
+    # batch_texts = ["book A text", "book B text", "book C text"]
+    batch_texts = []
+    book_ids = []
+    for book in bestsellers:
+        text = e5_service.build_text(book)
+        batch_texts.append(text)
+        book_ids.append(book['isbn13']) # 고유 ID, DB에 key로 사용
+    embeddings = e5_service.embed_batch({"text": batch_texts})["embedding"]
